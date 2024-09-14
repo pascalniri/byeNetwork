0137888fc4a18c775f334899606de324ef33b6ba
pascalniri <pascalniri@gmail.com>
2024-09-14 08:58:27 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import Navbar from './components/Navbar'
-import Projects from './components/Projects'
-// import Resources from './components/Resources'
+import React from 'react';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+// import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+// import Resources from './components/Resources';
 
 function App() {
-
   return (
-   <BrowserRouter>
-       <Navbar />
-   <Routes>
-    <Route  path='/' exact component ={<LandingPage />} />
-    <Route path='/projects' component ={<Projects />} />
-    {/* <Route path='/resources' element={<Resources />} /> */}
-   </Routes>
-   </BrowserRouter>
-  )
+    <Routes>
+      <Route exact path="/" element={<LandingPage />} />
+      <Route path="/projects" element={<Projects />} />
+      {/* <Route path="/resources" element={<Resources />} /> */}
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
+// Ensure there's a div with id "root" in your index.html
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
